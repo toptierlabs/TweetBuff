3f13f8d202b13e4ddb6095d2449a2bf321ad81f1
William Alton <bill@stackpointerllc.com>
2011-07-19 10:24:22 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetbuffer::Application.config.secret_token = '6330d5d9f07fbf5cd0045cb22239b713883f453d17694588eb1675df130e62362dac643628fe668fa77ea73369b32d4910f54f8a53648ad747aa68056e4a7f64'
