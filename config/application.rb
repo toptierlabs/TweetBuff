328f92fc0e20d67fc3bb27ac98aac3ff80728d64
William Alton <bill@stackpointerllc.com>
2011-07-19 10:25:19 -0400

    Set up app config

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,12 @@ module Tweetbuffer
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password]  + [:password_confirmation]
+  config.generators do |g|
+    g.stylesheets false
+    g.form_builder :simple_form
+    g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+  end
+
   end
 end
