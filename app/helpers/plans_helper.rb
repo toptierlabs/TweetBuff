402f7c0e6107424aa33b31c1f366f346dff5142f
dariel <dariel@41studio.com>
2011-10-19 15:16:36 +0700

    update plans layout#button monthly and year

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,2 +1,15 @@
 module PlansHelper
+
+  def format_price_monthly(price)
+    number_to_currency(price)
+  end
+  
+  def format_price_years(price)
+    price_year = price * 12
+    diskon = (price_year * 15).to_f / 100.to_f
+    sum = (price_year - diskon ).to_f / 12.to_f
+    
+    number_to_currency(sum.round(2))
+  end
+  
 end
