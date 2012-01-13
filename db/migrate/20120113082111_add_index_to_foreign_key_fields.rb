76488ce2dbfb39c1fcdbde8c0ec70317af41217e
Hendra Nicholas <hendra@41studio>
2012-01-13 16:08:22 +0700

    fix some bug

--- db/migrate/20120113082111_add_index_to_foreign_key_fields.rb
+++ db/migrate/20120113082111_add_index_to_foreign_key_fields.rb
@@ -7,12 +7,14 @@ class AddIndexToForeignKeyFields < ActiveRecord::Migration
     add_index :payment_notifications, [:cart_id, :transaction_id]
     add_index :plans_timeframes, [:plan_id, :timeframe_id]
     add_index :preferences, :user_id
-    add_index :subscriptions, [:user_id, :plan_id, :cart_id]
+    add_index :subcriptions, [:user_id, :plan_id, :cart_id]
     add_index :suggestions, [:category_id, :user_id, :twitter_user_id]
-    add_index :time_settings, [:timeframe_id, :twitter_user_id, :user_id]
+    add_index :time_settings, [:timeframe_id, :user_id]
+    add_index :time_settings, :twitter_user_id
     add_index :tweets, [:twitter_user_id, :buffer_preference_id]
     add_index :tweet_histories, [:user_id, :buffer_id]
-    add_index :tweet_intervals, [:user_id, :twitter_user_id, :timeframe_id]
+    add_index :tweet_intervals, [:user_id, :timeframe_id]
+    add_index :tweet_intervals, :twitter_user_id
     add_index :twitter_users, [:user_id, :twitter_id]
     add_index :users, :referal_id
   end
